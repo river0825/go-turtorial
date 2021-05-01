86f0e097c0183946242ed354ec8371e969a90ab2
river.lin <river.lin@xrspace.io>
2021-05-01 18:25:54 +0800

    make channel in equivalent binary trees unbuffered

--- concurrency/equivalentBinaryTrees.go
+++ concurrency/equivalentBinaryTrees.go
@@ -21,8 +21,8 @@ func Walk(t *tree.Tree, ch chan int) {
 }
 
 func Same(t1, t2 *tree.Tree) bool {
-	ch1 := make(chan int, 1)
-	ch2 := make(chan int, 2)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
 
 	go Walk(t1, ch1)
 	go Walk(t2, ch2)
